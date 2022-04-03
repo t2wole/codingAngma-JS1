41a7a0adbe3f29128ab69bce0e841423b113186b
t2wole <t2wole@gmail.com>
2022-04-03 13:35:59 +0900

    second

---
+++ practice03.js
@@ -0,0 +1,22 @@
+// const mathScore = prompt("수학 몇점?");
+// const engScore = prompt("영어 몇점?");
+// const result = (mathScore + engScore) / 2;
+
+// console.log(result);
+// 수학30 영어 50이면 --> 3050 
+// 이유는 prompt는 무조건 문자형
+
+// Number()
+console.log(Number("1234"));
+console.log(Number(true), Number(false));
+console.log(Number(null));
+console.log(Number(undefined));
+console.log("=========================================");
+
+// Boolean()
+console.log(Boolean(1), Boolean("javascript"));
+console.log(Boolean(0), Boolean(""), Boolean(null), Boolean(undefined), Boolean(NaN));
+console.log(Boolean('0'), Boolean(" "));
+console.log("=========================================");
+
+
